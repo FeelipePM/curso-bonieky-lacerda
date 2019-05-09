c38c01a27e93af23d572cc3b0bc04b72a81c4d23
Felipe Machado <feelipe@email.com>
2019-05-08 22:55:16 -0300

    versão mobile em funcionamento

--- mega-portal/js/index.js
+++ mega-portal/js/index.js
@@ -28,11 +28,9 @@ function slideChange(pos) {
 }
 
 function toggleMenu() {
-  var menu = document.getElementById("menu");
+  const menu = document.getElementById("menu");
 
-  if (menu.style.display == "none" || menu.style.display == "") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
+  menu.style.display == "none" || menu.style.display == ""
+    ? (menu.style.display = "block")
+    : (menu.style.display = "none");
 }
