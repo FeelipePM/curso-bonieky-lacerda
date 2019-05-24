a2b19abbd38eff3acc6562e9a7dbf52f3281b5a7
Felipe Machado <feelipe@email.com>
2019-05-24 15:11:22 -0300

    Menu mobile finalizado.

---
+++ medicenter/js/menu-mobile.js
@@ -0,0 +1,11 @@
+function menuMobile() {
+  const menuNavUl = document.querySelector(".menu-nav ul");
+
+  document.querySelector(".mobile-menu").addEventListener("click", function() {
+    menuNavUl.style.display == "flex"
+      ? (menuNavUl.style.display = "none")
+      : (menuNavUl.style.display = "flex");
+  });
+}
+
+menuMobile();
